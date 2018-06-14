b122793c417b51f286e8e16296ad006d1571a648
sinothilem <sinothilem@D987321>
2018-06-14 08:50:08 +0200

    done

---
+++ StringCalculatorKata/srs/StringCalculator.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Linq;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            var sum = 0;
+
+            if (IsEmpty(numbers))
+            {
+                return 0;
+            }
+
+            if (numbers.StartsWith("//["))
+            {
+                numbers = RemoveMultipleCustomDelimeters(numbers);
+            }
+
+            if (numbers.StartsWith("//"))
+            {
+                numbers = RemoveOneCustomDelimeter(numbers);
+            }
+
+            var numberArray = numbers.Replace('\n', ',').Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            ValidateNegativeNumbers(numberArray);
+
+            sum = numberArray.Where(x => int.Parse(x) <= 1000).Sum(x => int.Parse(x));
+            return sum;
+        }
+
+        private static string RemoveMultipleCustomDelimeters(string numbers)
+        {
+            var delimiter = numbers.Substring(3, numbers.IndexOf("]") - 3);
+            numbers = numbers.Replace("//", string.Empty)
+                     .Replace(delimiter, ",")
+                     .Remove(0, 3)
+                     .TrimStart('\n');
+
+            if (numbers.StartsWith("["))
+            {
+                numbers = RemoveMultipleCustomDelimeters($"//{numbers}");
+            }
+            return numbers;
+        }
+
+        private static string RemoveOneCustomDelimeter(string numbers)
+        {
+            var delimiter = numbers[2];
+            numbers = numbers.Replace("//", string.Empty)
+                                .Replace(delimiter, ',')
+                                .TrimStart(',')
+                                .TrimStart('\n');
+            return numbers;
+        }
+
+        public bool IsEmpty(string numbers)
+        {
+            return string.IsNullOrWhiteSpace(numbers);
+        }
+        public void ValidateNegativeNumbers(string[] numberArray)
+        {
+            var negativeNumbers = numberArray.Where(x => int.Parse(x) < 0);
+
+            if (negativeNumbers.Any())
+            {
+                var negativeNumber = string.Join(" ", negativeNumbers);
+                throw new Exception($"Negatives not allowed {negativeNumber}");
+            }
+        }
+
+    }
+}
---
+++ StringCalculatorKata/tests/StringCalculatorTests.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase(" ")]
+        public void Add_GivenAnEmptyNullAndWhiteSpaceNumber_ShouldReturn0(string numbers)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            int expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("4", 4)]
+        public void Add_GivenANumber_ShouldReturnANumber(string numbers, int expected)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase("4,3", 7)]
+        [TestCase("5,5", 10)]
+        [TestCase("2,13", 15)]
+        public void Add_GivenTwoNumbers_ShouldReturnTheSumOfNumbers(string numbers, int expected)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase("4,3,4", 11)]
+        [TestCase("5,5,5", 15)]
+        [TestCase("2,13,1", 16)]
+        public void Add_GivenThreeNumbers_ShouldReturnTheSumOfNumbers(string numbers, int expected)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Add_GivenNumbersWithNewLinesInBetween_ShouldReturnTheSumOfNumbers()
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+            var numbers = "1\n2,3";
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            var expected = 6;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//;\n1;2;5", 8)]
+        [TestCase("//;\n1\n0;2;5", 8)]
+        public void Add_GivenNumbersWithNewLineInBetweenandSingleDelimeter_ShouldReturnTheSumOfNumbers(string numbers, int expected)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+ 
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        
+        [TestCase("//[***]\n1***2***3" ,6)]
+        [TestCase("//[*]\n1*2*3", 6)]
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        [TestCase("//[**********][%]\n1**********2%%%%%%%%%%3", 6)]
+        public void Add_GivenNumbersWithNewLineInBetweenandAndMultipleDelimeters_ShouldReturnTheSumOfNumbers(string numbers, int expected)
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumbers_ShouldReturnNegativesNotAllowed()
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+            var numbers = "1\n-2,-3";
+
+            //Act
+            var expected = Assert.Throws<Exception>(() => createStringCalculator.Add(numbers));
+
+            //Assert
+
+            Assert.AreEqual("Negatives not allowed -2 -3", expected.Message);
+        }
+
+        [Test]
+        public void Add_GivenNumberGreaterThan1000_ShouldIgnoreThatNumberAndReturnTheSumOfNumbers()
+        {
+            //Arrange
+            var createStringCalculator = new StringCalculator();
+            var numbers = "1002,3";
+
+            //Act
+            var actual = createStringCalculator.Add(numbers);
+
+            //Assert
+            var expected = 3;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
